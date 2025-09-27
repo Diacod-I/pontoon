e6fdc7af9a8b962c131edefa1f395a5f69bf6bc9
Diacod-I <advithsonu@gmail.com>
2025-09-27 22:17:18 +0530

    remove vite package

--- convex/fetch_online_users.ts
+++
@@ -1,5 +0,0 @@
-import { db } from './_generated/server';
-
-export default async function fetchOnlineUsers() {
-  return await db.query('online_users').filter((q) => q.eq(q.field('online_flag'), 1)).collect();
-}
