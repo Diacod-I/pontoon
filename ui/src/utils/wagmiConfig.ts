a5d9716f38b908ae0ef90cf930f201a975de7e1a
Gurpreet <gs109690@gmail.com>
2025-09-27 18:52:56 +0530

    setup rainbowkit with flow

---
+++ ui/src/utils/wagmiConfig.ts
@@ -0,0 +1,36 @@
+'use client';
+
+import { connectorsForWallets } from '@rainbow-me/rainbowkit';
+import { createConfig, http } from 'wagmi';
+import { flowMainnet, flowTestnet } from 'viem/chains';
+import { flowWallet } from './flowWallet';
+
+
+const projectId = import.meta.env.VITE_PROJECT_ID;
+
+if (!projectId) {
+    throw new Error('NEXT_PUBLIC_PROJECT_ID is not defined');
+}
+
+const connectors = connectorsForWallets(
+  [
+    {
+      groupName: 'Recommended',
+      wallets: [flowWallet]
+    },
+  ],
+  {
+    appName: 'RainbowKit App',
+    projectId,
+  }
+);
+
+export const wagmiConfig = createConfig({
+  connectors,
+  chains: [flowTestnet, flowMainnet],
+  ssr: true,
+  transports: {
+    [flowMainnet.id]: http(),
+    [flowTestnet.id]: http(),
+  },
+});
