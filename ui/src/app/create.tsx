7da75d5d3b65eca46bfb95a32c7fad8dbec07504
Diacod-I <advithsonu@gmail.com>
2025-09-28 02:33:51 +0530

    feat: develop /create page and staking frontend

--- ui/src/app/create.tsx
+++ ui/src/app/create.tsx
@@ -1,4 +1,5 @@
 'use client'
+import Connect from "@/components/connect";
 import { useRouter } from 'next/navigation'
 
 export default function HomePage() {
@@ -6,6 +7,18 @@ export default function HomePage() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-br from-[#baeb34]/50 to-[#baeb34]">
+      
+      <nav className="bg-black/10 dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b-3 border-black dark:border-gray-600">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <div className="flex flexcol items-center justify-center mx-2">
+            <img src="./logo.png" className="h-16 border-black border-3 rounded-full mx-3" alt="Pontoon Logo"/>
+            <span className="text-2xl font-semibold dark:text-white mx-auto">Pontoon</span>
+            </div>
+          <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+            <Connect />
+          </div>
+        </div>
+      </nav>
       {/* <div className="bg-[url('/monkey_background.png')] bg-cover bg-center z-0 h-screen"/> */}
       <div className="container flex flex-col items-center justify-center gap-6 py-8 mb-20 mt-15">
         <div className="bg-white border-black border-3 px-6 rounded-md mb-2 text-animation">
